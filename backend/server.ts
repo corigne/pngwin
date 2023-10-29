6113498e40cdf8d500ff6e78669080a6226c77d1
Nathan Jodoin <corigne@gmail.com>
2023-10-29 17:40:52 -0500

    Got OTP route working. Need JWT and login functionality, but this should be good for registration and login.

--- backend/server.ts
+++ backend/server.ts
@@ -7,6 +7,8 @@ import Session from './models/Session.model'
 import { createTransport } from 'nodemailer'
 import Email from 'email-templates'
 import { v4 as uuidv4 } from 'uuid';
+import { warn } from 'console'
+import { stringify } from 'querystring'
 
 //import jwt from 'jsonwebtoken'
 const jwt = require('jsonwebtoken')
@@ -19,22 +21,22 @@ app.use(express.json())
 
 // Create a SMTP transporter object
 var transporter = createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
-    auth: {
-        user: process.env.GMAIL,
-        pass: process.env.GMAIL_PASS
-    }
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+    user: process.env.GMAIL,
+    pass: process.env.GMAIL_PASS
+  }
 })
 
 const email_message = new Email({
- message: {
-   from: process.env.GMAIL
- },
- send: true,
- transport: transporter,
- preview: false
+  message: {
+    from: process.env.GMAIL
+  },
+  send: true,
+  transport: transporter,
+  preview: false
 })
 
 // Option 1: Passing a connection URI
@@ -50,37 +52,37 @@ const sequelize = new Sequelize({
 const privateKey = process.env.RSA_PRIV_KEY
 
 app.get('/.well-known/jwks.json', (res: Response) => {
-    try {
-        const modulus = process.env.RSA_KEY_N
-        const exponent = process.env.RSA_KEY_E
-        const kid = process.env.RSA_KEY_KID
+  try {
+    const modulus = process.env.RSA_KEY_N
+    const exponent = process.env.RSA_KEY_E
+    const kid = process.env.RSA_KEY_KID
 
-        if(!modulus || !exponent || !kid){
-            throw new Error('RSA key informatiion is missing')
-        }
-        const jwksKey: JwksKey = {
-            kid,
-            kty: 'RSA',
-            alg: 'RS256',
-            use: 'sig',
-            n: modulus,
-            e: exponent,
-        }
-
-        const jwks: Jwks = {
-            keys: [jwksKey],
-        }
-
-        res.json(jwks)
-    } catch (error) {
-        res.status(500).json({error: 'Internal Server Error'})
+    if(!modulus || !exponent || !kid){
+      throw new Error('RSA key informatiion is missing')
     }
- })
+    const jwksKey: JwksKey = {
+      kid,
+      kty: 'RSA',
+      alg: 'RS256',
+      use: 'sig',
+      n: modulus,
+      e: exponent,
+    }
+
+    const jwks: Jwks = {
+      keys: [jwksKey],
+    }
+
+    res.json(jwks)
+  } catch (error) {
+    res.status(500).json({error: 'Internal Server Error'})
+  }
+})
 
 var issue_JWT = (userid: number, session_id: number, length_days: number) => {
   // create a JWT with the user_id, session_id, role, iat, and exp baked in
   // returns JWT or null
- }
+}
 
 var verify_JWT = (token: JSON) => {
   // verify the user's JWT is valid, return true if valid, false if not
@@ -167,29 +169,83 @@ app.post('/api/verifyOTP', async (req: Request, res: Response) => {
     missing += (body.session_id) ? " " : "session_id "
     return res.status(418).json({session_verified: false, reason: "Missing:" + missing})
   }
+  const OTP = body.OTP
+  const session_id = body.session_id
 
   // validates OTP with session_id
-  try{
-    await Session.findOne({
-      where:{
-        session_id: body.session_id
-      }
-    })
-      .then((session) => {
-        // TODO if the OTP has expired, return an Error stating the OTP has expired
+  await Session.findOne({
+    where:{
+      session_id: session_id
+    }
+  })
+  .then((session) => {
 
-        // TODO else return a valid JWT
-        // TODO need to return the session JWT if the OTP is valid and the session
-        return res.status(200).json(session)
+    if (!session){
+      return res.status(400).json({
+        verified: false,
+        token: null,
+        reason: "Session not found."
       })
-  }
-  catch(err: any){
-    return res.status(400).json({session_verified: false, reason: "Session_id does not exist: ", error: err})
-  }
+    }
 
-  // TODO if valid issue JWT and return valid: true and jwt:token
+    // TODO DELETEME
+    console.log(session)
 
-  // TODO if invalid return valid: false and jwt:null
+    if(session?.dataValues.pending === false){
+      return res.status(401).json({
+        verified: false,
+        token: null,
+        reason: "Session is already verified or expired."
+      })
+    }
+
+    if(OTP === session?.dataValues.otp){
+      console.log("OTP Matched")
+
+      var created: Date = new Date(session?.dataValues.created_on.toString())
+
+      const elapsed_min: number = (Date.now() - created.getTime()) /1000/60
+      console.log(`Elapsed time in min: ${elapsed_min}`)
+      // Check for expired OTP Session
+      // if (elapsed_min > 5){
+      if (!true){
+        // return an Error stating the OTP has expired
+        return res.status(401).json({
+          verified: false, token: null, reason: "OTP Expired"
+        })
+      }
+
+      else{
+        // validate the session
+        // generate the jwt and store it in the session
+        try{
+          session.set({
+            pending : false,
+            valid : true,
+            token : JSON.parse('{ "test": "placeholder" }')
+          })
+          session.save()
+          .then((result) => {
+            console.log(result)
+            return res.status(200).json(result)
+          })
+        }
+        catch(err: any){
+          console.log("Database Error:" + err.toString())
+          return res.status(500).json(err)
+        }
+      }
+    }
+    else{
+      console.log("OTP Did Not Match")
+      return res.status(418).json({
+        verified: false, token: null, reason: "Invalid OTP"
+      })
+    }
+  })
+  .catch((err) => {
+    return res.status(400).json({session_verified: false, reason: "Session_id does not exist: ", error: err.toString()})
+  })
 })
 
 app.post('/api/testSession', async (req: Request, res: Response) => {
@@ -201,12 +257,12 @@ app.post('/api/testSession', async (req: Request, res: Response) => {
   }
 
   await create_session(body.user_id)
-    .then((id) => {
-      res.status(200).json({session_created: true, session_id: id})
-    })
-    .catch((err) => {
-      res.status(400).json({session_created: false, reason: err.toString()})
-    })
+  .then((id) => {
+    res.status(200).json({session_created: true, session_id: id})
+  })
+  .catch((err) => {
+    res.status(400).json({session_created: false, reason: err.toString()})
+  })
 
 })
 
@@ -259,23 +315,23 @@ app.post('/api/createUser', async (req: Request, res: Response) => {
 
 // login route
 app.post('/api/auth', async (req: Request, res: Response) => {
-    const username = 'username'
-    const password = 'password'
+  const username = 'username'
+  const password = 'password'
 
-    const {body} = req
+  const {body} = req
 
-    if(!body.username || !body.password) {
-        return res.status(400).json({message: `Username and password are necessary.`})
-    }
+  if(!body.username || !body.password) {
+    return res.status(400).json({message: `Username and password are necessary.`})
+  }
 
-    if(body.username === username && body.password === password) {
+  if(body.username === username && body.password === password) {
 
-        //const token = jwt.sign({ username: body.username }, privateKey , { expiresIn: '1h', algorithm: "RS256" })
+    //const token = jwt.sign({ username: body.username }, privateKey , { expiresIn: '1h', algorithm: "RS256" })
 
-        return res.json({message: 'Auth successful'})
-    } else {
-        return res.status(401).json({message: 'Auth failed, invalid credentials.'})
-    }
+    return res.json({message: 'Auth successful'})
+  } else {
+    return res.status(401).json({message: 'Auth failed, invalid credentials.'})
+  }
 
 })
 
@@ -297,7 +353,7 @@ app.get('/api', async (req: Request, res: Response) => {
 })
 
 app.listen(port, () => {
-    console.log(`Running on http://locahost:${port}`)
+  console.log(`Running on http://locahost:${port}`)
 })
 
-//
+  //
