45365358a6cd029e103f089880e0fb31ceafe523
Alex Ho <hoalex219@gmail.com>
2023-11-15 04:21:04 -0600

    Login button actually calls api correctly at least for just base user login, just need a modal for OTP

--- frontend/src/lib/LogInButton/+page.svelte
+++ frontend/src/lib/LogInButton/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import SignUpModal from "$lib/VerifyModal/+page.svelte";
+    import { onMount } from "svelte";
     import {
         Modal,
         ModalBody,
@@ -9,8 +10,41 @@
         Input,
         InputGroup,
     } from "sveltestrap";
+
     let open = false;
+    let username = '';
+    let inputField;
+    let rememberMe = false;
+
     const toggle = () => (open = !open);
+    const login = () => {
+        //fetch request to api/login through localhost:3000/api/login with username from input
+        //if successful, set cookie and redirect to home
+        //else, display error message
+        console.log(username);
+        fetch('http://localhost:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username: username, jwt: null, remembered: rememberMe})
+        })
+        .then(response => response.json())
+        //log response
+        .then(data => {
+            console.log(data);
+        })
+    }
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        login();
+    }
+    onMount(() => {
+        //focus on username input
+        if(open && inputField){
+            inputField.focus();
+        } 
+    });
 </script>
 
 <Button class="login" color="warning" on:click={toggle}>Login</Button>
@@ -18,15 +52,23 @@
     <Modal isOpen={open} backdrop={false} {toggle}>
         <ModalHeader {toggle}>Login</ModalHeader>
         <ModalBody>
-            <h6>Username</h6>
-            <InputGroup>
-                <Input placeholder="Ex. _bbygworlpngwin" />
-            </InputGroup>
+            <form on:submit={handleFormSubmit}>
+                <h6>Username</h6>
+                <InputGroup>
+                    <Input bind:value={username} placeholder="Ex. _bbygworlpngwin" />
+                </InputGroup>
+
+                <label>
+                    <input type="checkbox" bind:checked={rememberMe} />
+                    Remember Me
+                </label>
+                
+                <ModalFooter>
+                    <SignUpModal />
+                    <Button color="warning" type="submit">Login</Button>
+                    <Button color="secondary" on:click={toggle}>Cancel</Button>
+                </ModalFooter>
+            </form>
         </ModalBody>
-        <ModalFooter>
-            <SignUpModal />
-            <Button color="warning">Submit</Button>
-            <Button color="secondary" on:click={toggle}>Cancel</Button>
-        </ModalFooter>
     </Modal>
 </div>
